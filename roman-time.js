1cf862c87713bd3a47ed30e95622eeeebb79860c
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-10-07 19:22:06 +0500

    Третий блин

--- roman-time.js
+++ roman-time.js
@@ -1,53 +1,47 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+'use strict';
+
+var hours = parseInt(process.argv[2]);
+var minutes = parseInt(process.argv[3]);
 // Немного замечательного кода и магии
 
-if (parseInt(hours) > 23 || parseInt(hours) < 0 || parseInt(minutes) > 59 || parseInt(minutes) < 0) {
-    console.log("Время указано неверно!");
-}
-else {
+if (hours > 23 || hours < 0 || minutes > 59 || minutes < 0) {
+    console.log('Время указано неверно!');
+} else {
     var romanTime = {
-        "1": "I",
-        "2": "II",
-        "3": "III",
-        "4": "IV",
-        "5": "V",
-        "6": "VI",
-        "7": "VII",
-        "8": "VIII",
-        "9": "IX",
-        "10": "X",
-        "20": "XX",
-        "30": "XXX",
-        "40": "XL",
-        "50": "L"
+        0: 'NN', // у ноля нет обозначения, так что он будет N от своего латинского названия
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X',
+        20: 'XX',
+        30: 'XXX',
+        40: 'XL',
+        50: 'L'
     };
-    var romanHours = "";
-    var romanMinutes = "";
+    var romanHours = '';
+    var romanMinutes = '';
 
-    if (parseInt(hours) === 0) {
-        romanHours = "NN"; // у ноля нет обозначения, так что он будет N от своего латинского названия
-    }
-    else if (parseInt(hours) % 10 !== 0) {
-        var temp = (Math.floor(parseInt(hours) / 10) * 10).toString();
-        var temp2 = (parseInt(hours) % 10).toString();
+    if (hours % 10 !== 0) {
+        var temp = Math.floor(hours / 10) * 10;
+        var temp2 = hours % 10;
         romanHours = romanTime[temp] + romanTime[temp2];
-    }
-    else {
+    } else {
         romanHours = romanTime[hours];
     }
 
-    if (minutes === "00") {
-        romanMinutes = "NN";
-    }
-    else if (parseInt(minutes) % 10 !== 0) {
-        var temp = (Math.floor(parseInt(minutes) / 10) * 10).toString();
-        var temp2 = (parseInt(minutes) % 10).toString();
+    if (minutes % 10 !== 0) {
+        var temp = Math.floor(minutes / 10) * 10;
+        var temp2 = minutes % 10;
         romanMinutes = romanTime[temp] + romanTime[temp2];
-    }
-    else {
+    } else {
         romanMinutes = romanTime[minutes];
     }
 
-    console.log(romanHours + ":" + romanMinutes);
+    console.log(romanHours + ':' + romanMinutes);
 }
